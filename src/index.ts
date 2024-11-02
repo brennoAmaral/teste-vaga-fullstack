ab6e1b499f099edefcddecb27e8a82de074aa315
brennoAmaral <brenno.amaral.marques@gmail.com>
2024-11-02 02:13:24 -0300

    feat: index

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { FactoryContractCsvParser } from './app/csv-parser';
+
+const test = await FactoryContractCsvParser().execute('./src/data.csv')
+
+//customize aqui a visualização dos valores
+
+console.log(test[test.length-1])
